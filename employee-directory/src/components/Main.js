7c69da26b83d52eef18dad48d40d9eb50b7d8254
Admin <admin@Admins-MacBook-Pro.local>
2020-05-03 22:34:25 -0600

    Created a main file that render employeeContainer

---
+++ employee-directory/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EmployeeContainer from "./EmployeeContainer";
+import "../styles/Main.css";
+
+function Main() {
+  return (
+    <>
+      <EmployeeContainer />
+    </>
+  );
+}
+
+export default Main;
